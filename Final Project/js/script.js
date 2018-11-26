53b261405261534ae2d39757e7832f48adc52d0f
viscoseillusion <43328136+viscoseillusion@users.noreply.github.com>
2018-11-26 18:42:45 -0500

    updated

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -1,3 +1,13 @@
+$('nav a').hide().removeClass('.dropdown');
+$('#vi-lab').hover(
+	function() {
+		$('div', this).stop().slideDown(100);
+	},
+	function() {
+		$('div', this).stop().slideUp(100);
+	}
+	);
+
 /* $('#vi-lab').hover()
 
 function dropDown() {
